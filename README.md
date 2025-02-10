cd8b092d78c63e86ccfbb9e58e44005ff277a9f9
arikaim <info@arikaim.com>
2025-02-10 15:07:27 +0200

    =

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 #### Created With 
     * Tailwind CSS
+    * FlyonUi
 
 #### Customization 
 
@@ -13,9 +14,17 @@ In theme folder:
 
 1. Install Tailwind CSS
     npm install tailwindcss @tailwindcss/cli
+
 2. Install typography
     npm install @tailwindcss/typography
 
+3. Install Flyonui
+    npm install flyonui
+    
 4. Start watch process
-    npx @tailwindcss/cli -i ./css/include.css -o ./css/tailwind.min.css --watch
+    npx @tailwindcss/cli -i ./css/include.css -o ./css/tailwind.min.css --watch --minify
+ 
+
+npx tailwindcss -i css/include.css -o css/tailwind.min.css -m --watch 
+
  
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,7 @@ module.exports = {
     content: [
         './**/*.html',
         './**/styles.json',
-        './themes/default.json',
-        '../../components/tailwind/**/*.html',
-        '../../components/tailwind/**/*.json'
+        './themes/default.json'
     ], 
     theme: {
         screens: {
@@ -15,15 +13,28 @@ module.exports = {
         },
         extend: {},
     },
-    variants: {
-        extend: {
-            backgroundColor: ({ after }) => after(['disabled'])
-        },
-    },
     darkMode: 'class',
     plugins: [               
-        require("@tailwindcss/typography")
+        require("@tailwindcss/typography"),
+        require("flyonui")
     ],
+    flyonui: {
+        themes: [
+            {
+                "saas": {
+                    ...require("flyonui/src/theming/themes")["light"],
+                    primary: "#0ea5e9" // sky-500
+                } 
+            }
+        ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "soft"]
+        darkTheme: "dark", // name of one of the included themes for dark mode
+        base: false, // applies background color and foreground color for root element by default
+        styled: true, // include FlyonUI colors and design decisions for all components
+        utils: true, // adds responsive and modifier utility classes
+        vendors: false, // default is false when true add customize css for apexChart, editor.js, flatpickr, fullcalendar, notyf, raty-js
+        logs: true, // Shows info about FlyonUI version and used config in the console when building your CSS
+        themeRoot: ":root" // The element that receives theme color CSS variables
+    },
     corePlugins: {    
         preflight: false  
     }
